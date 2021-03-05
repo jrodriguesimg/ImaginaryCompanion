5c8ecf1fdd30b976a0c8b1d5c1b1fd0901451122
Flavio Andrade <fandrade@imaginarycloud.com>
2021-03-05 17:56:29 +0000

    Fix action layout

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ExtensionPopup from './components/ExtensionPopup';
 import reportWebVitals from './reportWebVitals';
 
 
